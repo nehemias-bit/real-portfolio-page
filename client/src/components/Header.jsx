745d1dabada45f30aaf975ecddbf5a3b141bd064
Nehemias Cruz <nehemiascruz@Nehemiass-MacBook-Pro.local>
2019-12-16 19:55:26 -0500

    updated resume to docdroid doc

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -10,12 +10,12 @@ export default function Header(props) {
             <div id="header-anchor">
               <a href="#contact" id="contactme-one">Contact Me</a>
               <a href="#projects-section" id="projects-in">Projects</a>
-              <a href={NehemiasResume} target="_blank" rel="noopener noreferrer" id="about-me">About Me</a>
+              <a href="https://docdro.id/AOW8f68" target="_blank" rel="noopener noreferrer" id="about-me">About Me</a>
             </div> :
             <div id="header-anchor-two">
               <a href="#contact" id="contactme-two">Contact Me</a>
               <a href="#" id="projects-out">Projects</a>
-              <a href={NehemiasResume} target="_blank" rel="noopener noreferrer" id="about-me-two">About Me</a>
+              <a href="https://docdro.id/AOW8f68" target="_blank" rel="noopener noreferrer" id="about-me-two">About Me</a>
             </div>
         }
       </div>
