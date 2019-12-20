4e95f3daf5737be2b8b12b9e0102c2397bf75350
Nehemias Cruz <nehemiascruz@Nehemiass-MacBook-Pro.local>
2019-12-20 16:04:56 -0500

    updated the modal css and the header and the resume

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -9,12 +9,12 @@ export default function Header(props) {
             <div id="header-anchor">
               <a href='mailto:nemicruz003@gmail.com' target="_blank" rel="noopener noreferrer" id="contactme-one">Contact Me</a>
               <a href="#projects-section" id="projects-in">Projects</a>
-              <a href="https://docdro.id/AOW8f68" target="_blank" rel="noopener noreferrer" id="about-me">About Me</a>
+              <a href="https://docdro.id/8CPDUw8" target="_blank" rel="noopener noreferrer" id="about-me">About Me</a>
             </div> :
             <div id="header-anchor-two">
               <a href="#contact" id="contactme-two">Contact Me</a>
               <a href="#" id="projects-out">Projects</a>
-              <a href="https://docdro.id/AOW8f68" target="_blank" rel="noopener noreferrer" id="about-me-two">About Me</a>
+              <a href="https://docdro.id/8CPDUw8" target="_blank" rel="noopener noreferrer" id="about-me-two">About Me</a>
             </div>
         }
       </div>
--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -25,11 +25,8 @@ export default class Modal extends Component {
                 This project was created using the React framework, a part of the Yelp api, and the freegeoip geo-location api. The page renders and automatically shows you restaurants near your location. Additionally, you are able to search for restaurants in other cities and other countries(up to 32 different countries). 
               </p>
               <div className="modal-content">
-              <video src="https://i.imgur.com/DN9MR8W.mp4" width="240" height="149" autoplay="true" CONTROLLER="false" loop="true" PLUGINSPAGE="http://www.apple.com/quicktime/"></video>
+              <video id="sondar-video" src="https://i.imgur.com/DN9MR8W.mp4" autoplay="true" CONTROLLER="false" loop="true" PLUGINSPAGE="http://www.apple.com/quicktime/"></video>
                 <a href="https://sondar.surge.sh/" target="_blank" rel="noopener noreferrer">visit site</a>
-                <i class="devicon-react-original"></i>
-                <i class="devicon-html5-plain-wordmark colored"></i>
-
               </div>
           </div> : null  
         }
@@ -39,8 +36,14 @@ export default class Modal extends Component {
           <button onClick={e => {
             this.props.closeModal(e);
               }}>X</button>
+              <div id="modal-two-h1-div">
+                <h1>Park Vision</h1>
+              </div>
               <p>Parkvision will show information about different park events going on in New York at the time of your visit to the site. It uses the National Parks Service API and if you provide a two-letter state abbreviation it will diplay popular images of six different parks from that state as well as links to their respective websites.</p>
-              <a href="http://parkvision.surge.sh/" target="_blank" rel="noopener noreferrer">Parkvision</a>
+              <div className="modal-content">
+                <video id="parkvision-video" src="https://i.imgur.com/yIDtjzN.mp4" autoplay="true" CONTROLLER="false" loop="true" PLUGINSPAGE="http://www.apple.com/quicktime/"></video>
+                <a href="http://parkvision.surge.sh/" target="_blank" rel="noopener noreferrer">visit site</a>
+              </div>  
           </div> : null
         }
         {
@@ -49,8 +52,14 @@ export default class Modal extends Component {
           <button onClick={e => {
             this.props.closeModal(e);
               }}>X</button>
+              <div id="modal-three-h1-div">
+                <h1>Undisclosed</h1>
+              </div>
               <p>The purpose of this app is to allow the user to create blogs of places in their city that are secret or not very well known. Register, login, and you'll be able to see the locations that other user's have posted.</p>
-              <a href="http://undisclosed.surge.sh/" target="_blank" rel="noopener noreferrer">Undisclosed</a>
+              <div className="modal-content">
+                <video id="undisclosed-video" src="https://i.imgur.com/D2mNbRb.mp4" autoplay="true" CONTROLLER="false" loop="true" PLUGINSPAGE="http://www.apple.com/quicktime/"></video>
+                <a href="http://undisclosed.surge.sh/" target="_blank" rel="noopener noreferrer">visit site</a>
+              </div>  
           </div> : null
         }
         {
@@ -59,8 +68,14 @@ export default class Modal extends Component {
           <button onClick={e => {
             this.props.closeModal(e);
           }}>X</button>
-              <p>This abook is for the readers. Register and login and you'll be able to create a list of books that you are planning on reading at some point. If you read something particularly interesting, attach a not to that book so that you can reference it later. Once done with the book you're currently on hit the finished button to transfer your book from the next/current to the finished section.</p>
-          <a href="http://abook.surge.sh/login" target="_blank" rel="noopener noreferrer">abook</a>
+          <div id="modal-four-h1-div">
+            <h1>abook</h1>
+          </div>
+          <p>This abook is for the readers. Register and login and you'll be able to create a list of books that you are planning on reading at some point. If you read something particularly interesting, attach a not to that book so that you can reference it later. Once done with the book you're currently on hit the finished button to transfer your book from the next/current to the finished section.</p>
+          <div className="modal-content">
+            <video id="abook-video" src="https://i.imgur.com/VuUdSwb.mp4" autoplay="true" CONTROLLER="false" loop="true" PLUGINSPAGE="http://www.apple.com/quicktime/"></video>
+            <a href="http://abook.surge.sh/login" target="_blank" rel="noopener noreferrer">visit site</a>
+          </div>      
           </div> : null
         }
 
